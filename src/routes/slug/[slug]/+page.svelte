4f77557ac875aa0f024417f7f4f84309ed19fb3d
Jakub Klimkiewicz <jakubklimkiewicz70@gmail.com>
2024-03-19 17:02:23 +0100

    load test

---
+++ src/routes/slug/[slug]/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+
+	/** @type {import('./$types').PageData} */
+	export let data: any;
+    const { title,
+            brand,
+            category,
+            description,
+            price,
+            images,
+            rating,
+            stock } = data.post.data.data
+
+    onMount(() => {
+        console.log(data.post.data)
+    })
+
+</script>
+
+<div class="flex justify-center items-center flex-col text-center gap-y-5">
+    <h1>{data.post.title}</h1>
+    <h3>{ title }</h3>
+    <div>{ brand }</div>
+    <div>{ description }</div>
+    <div>{ price }$</div>
+    {#each images as image_src, index}
+        <img src={image_src} alt={`image-${index}`}>
+    {/each}
+    <div>{ rating } ⭐</div>
+    <div>{ stock } </div>
+    <div>{ category } </div>
+</div>
---
+++ src/routes/slug/[slug]/+page.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+/** @type {import('./$types').PageLoad} */
+export async function load({ params }) {
+
+	const data = await axios.get(`https://dummyjson.com/products/${params.slug}`)
+
+	const content = `Content for ${params.slug} goes here`
+	const title = `Title for ${params.slug} goes here`
+
+	return {
+		post: {
+			title,
+			content,
+			data
+		}
+	};
+}
